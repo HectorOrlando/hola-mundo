8dfae29c7026d21e9b60ee9ec33c891f6dc9b230
Hector Orlando <14orgui@gmail.com>
2022-02-15 22:00:13 +0100

    cambios para el deploy
    pasos para el build y el deploy

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>HOLA MUNDO PARA ENVIAR AL SERVIDOR RASPBERRY PI 4</h2>
+        <a href="https://www.youtube.com/watch?v=OUsC13AEiUQ&ab_channel=willy2" target="_blank">Como subir tu aplicación React.js a GitHub Pages paso a paso | Como hacer deploy / npm run build</a>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
